69663f260e97c9008d9eed9451e4ba69f8213367
kevinccbsg <kevinccbsg@gmail.com>
2023-07-18 19:26:49 +0200

    feat: add switch example

--- examples/04_control.js
+++ examples/04_control.js
@@ -21,3 +21,41 @@ const resultado = edad === 30; // true o un false
 const message = resultado ? 'Tienes mi edad' : 'No tienes mi edad';
 
 console.log(message);
+
+console.log('<---example--->');
+
+let value = Infinity;
+
+if (value) {
+  console.log(`Value: ${value}`);
+} else {
+  console.log('Else ->', value);
+}
+
+// switch
+let day = "Wednesday";
+
+if (day === "Monday") {
+  console.log("It's monday");
+} else if (day === "Tuesday") {
+  console.log("It's Tuesday");
+} else if (day === "Wednesday") {
+  console.log("It's Wednesday");
+} else {
+  console.log("It is neither Monday, Tuesday nor Wednesday");
+}
+
+switch (day) {
+  case "Monday":
+    console.log("It's monday switch");
+    break;
+  case "Tuesday":
+    console.log("It's Tuesday switch");
+    break;
+  case "Wednesday":
+    console.log("It's Wednesday switch");
+    break;
+  default:
+    console.log("It is neither Monday, Tuesday nor Wednesday switch");
+    break;
+}
