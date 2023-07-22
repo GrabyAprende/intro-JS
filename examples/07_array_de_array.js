c7afad08887c06335c2ef32d9291ec526298327c
Gabriela Aguilera <gabrielaaguilera@Gabrielas-MacBook-Pro.local>
2023-07-22 16:07:35 +0200

    ejercicios de array de array,piedra, papel o tijera

--- examples/07_array_de_array.js
+++ examples/07_array_de_array.js
@@ -4,8 +4,41 @@ const matrix = [
   [7, 8, 9]  // 2
 ];
 
-console.log(matrix[1][0]);
+//console.log(matrix[1][0]);
 
 matrix.pop();
 
-console.log(matrix);
+//console.log(matrix);
+console.log("---matrix----")
+for (let index = 0; index < matrix.length; index++) {
+  const row = matrix[index];
+  console.log(row);
+
+  for (let column = 0; column < row.length; column++){
+console.log(row[column])
+  }
+}
+console.log("---end-matrix----")
+
+// Crear un array bidimensional con 5 columnas y con 3 filas como las que aparecen abajo
+/*
+--*--
+-***-
+*****
+
+Luego de definirlo recorrerlo para que se pinte como en el dibujo
+*/
+
+const arrayBi = [
+  ['-', '-', '*', '-', '-'], // 0
+  ['-', '*', '*', '*', '-'], // 1
+  ['*', '*', '*', '*', '*']  // 2
+];
+for (let i = 0; i < arrayBi.length; i++) {
+  let dibujo = "";
+
+  for (let j = 0; j < dibujo.length; j++) {
+    dibujo += arrayBi[i][j];
+}
+}
+console.log(dibujo);
--- examples/08_loops.js
+++ examples/08_loops.js
@@ -4,21 +4,93 @@ console.log('Inicio');
 // for (let i = 5; i > 0; i = i - 1) {
 //  console.log('Bloque dentro del for');
 //}
-let count = [];
+/*let count = [];
 for (let i = 0; i < 5; i++) {
-  console.log('Valor del i: ', i);
-  count.push(i);
+    console.log('Valor del i: ', i);
+    count.push(i);
 }
 
-console.log('FIN', count);
+//console.log('FIN', count);
 const fruits = ['apple', 'banana', 'kiwi', 'watermelon', 'orange', 'Kiwi'];
 
 for (let i = 0; i < fruits.length; i++) {
-  if (fruits[i].toLowerCase() !== 'kiwi') {
-    console.log(fruits[i]);
-  } else {
-    console.log('****');
-  }
+    if (fruits[i].toLowerCase() !== 'kiwi') {
+        console.log(fruits[i]);
+    } else {
+        console.log('****');
+    }
+}*/
+
+console.log("--continue y break--");
+for (let index = 0; index < 10; index++) {
+    if (index === 2 || index === 8 ){
+        continue; //para saltarse pasos del bucle
+    }
+    console.log("index", index);
 }
+console.log("-----end continue/break example------")  
+//reemplazar el continue por break, para parar.
+
+/*for (let index = 0; index < fruits.length; index++) {
+    const element = fruits[index];
+    if (fruits === orange){
+
+    }
+    
+}*/
+/*Enunciado: Dada una lista de números, crea una nueva lista que contenga únicamente los números impares de la lista original.
+
+Lista de números: 1, 2, 33, 41, 5, 60, 74, 87, 90, 101
+
+Paso 1: Crea un array llamado numbers que contenga al menos estos números
+Paso 2: Crea un array vacío llamado oddNumbers.
+Paso 3: Utiliza un bucle for para iterar sobre cada número en la lista numbers.
+Paso 4: En cada iteración, verifica si el número actual es impar.
+        Si es impar, agrégalo al array oddNumbers utilizando el método push().
+        NOTA: números pares son aquellos que al dividirlos por 2 el residuo/resto es 0
+Paso 5: Después del bucle, imprime en la consola el contenido del array oddNumbers.*/
+
+let numbers = [1, 2, 33, 41, 5, 60, 74, 87, 90, 101];
+let oddNumbers = [];
+for (let index = 0; index < numbers.length; index++) {
+    const number = numbers[index];
+    if (number[index] % 2 !== 0){
+        oddNumbers.push(number);
+    }  
+}
+console.log(oddNumbers);
 
 /// while
+let count = [];
+
+/*for (let i = 0; i <= 5; i++){
+    count.push(i);
+}*/
+let i = 0;
+while(i <= 5){ //si la i es menor a 5
+console.log(i);
+//para que no se quede un bucle infinito, se incrementa la i
+// i = i + 1;
+i++;
+count.push(i)
+}
+console.log(count);
+
+/*while(true) {
+    const mensaje = prompt("¿exit? Y o N?")
+    if (mensaje === "Y") break;
+    console.log(mensaje);
+    }*/
+
+    while(true) {
+        const pregunta = prompt("Elije: Piedra, papel o tijera")
+        const rival = "piedra"
+        if (pregunta === null)
+        if(pregunta === "papel") {
+            console.log("Ganaste, tu tienes papel y el rival piedra");   
+        } else if (pregunta === "tijeras") {
+            console.log("Perdiste, tu tienes tijeras y el rival piedra");
+        } else if (pregunta === "piedra") {
+            console.log("Empate, ambos tienen piedra");
+    } 
+}
